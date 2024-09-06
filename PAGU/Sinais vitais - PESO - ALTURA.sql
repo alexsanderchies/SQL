91f69be24eea51978a728424f519dda65fb23ff1
alexsandermv <alexsander.chies@mv.com.br>
2024-09-06 14:50:22 -0300

    .

--- PAGU/Sinais vitais - PESO - ALTURA.sql
+++ PAGU/Sinais vitais - PESO - ALTURA.sql
@@ -2,12 +2,26 @@ SELECT coleta_sinal_vital.cd_atendimento
       ,coleta_sinal_vital.data_coleta
       ,sinal_vital.ds_sinal_vital
       ,itcoleta_sinal_vital.valor
-      ,pw_unidade_afericao.ds_unidade_afericao 
+      ,pw_unidade_afericao.ds_unidade_afericao
+      ,SINAL_VITAL.CD_SINAL_VITAL
+      ,CASE 
+          WHEN PW_UNIDADE_AFERICAO.DS_UNIDADE_AFERICAO = 'CENTÍMETRO' THEN valor
+          WHEN PW_UNIDADE_AFERICAO.DS_UNIDADE_AFERICAO = 'METRO'      THEN VALOR * 100
+      END ALTURA_CM
   FROM dbamv.sinal_vital
   JOIN dbamv.itcoleta_sinal_vital 
     ON sinal_vital.cd_sinal_vital = itcoleta_sinal_vital.cd_sinal_vital
   JOIN dbamv.coleta_sinal_vital -- registro da coleta vinculado ao atendimento
     ON coleta_sinal_vital.cd_coleta_sinal_vital = itcoleta_sinal_vital.cd_coleta_sinal_vital
   JOIN dbamv.pw_unidade_afericao
-    ON itcoleta_sinal_vital.cd_unidade_afericao = pw_unidade_afericao.cd_unidade_afericao 
-  WHERE sinal_vital.ds_sinal_vital like '%PESO%'
+    ON ITCOLETA_SINAL_VITAL.CD_UNIDADE_AFERICAO = PW_UNIDADE_AFERICAO.CD_UNIDADE_AFERICAO 
+  WHERE SINAL_VITAL.CD_SINAL_VITAL = 10                  
+    AND (COLETA_SINAL_VITAL.CD_COLETA_SINAL_VITAL, COLETA_SINAL_VITAL.CD_ATENDIMENTO) IN (SELECT Max(COLETA_SINAL_VITAL.CD_COLETA_SINAL_VITAL)
+                                                                                                ,COLETA_SINAL_VITAL.CD_ATENDIMENTO
+                                                                                            FROM DBAMV.COLETA_SINAL_VITAL
+                                                                                            JOIN DBAMV.ITCOLETA_SINAL_VITAL
+                                                                                              ON coleta_sinal_vital.cd_coleta_sinal_vital = itcoleta_sinal_vital.cd_coleta_sinal_vital
+                                                                                            WHERE itcoleta_sinal_vital.CD_SINAL_VITAL = 10
+                                                                                              AND COLETA_SINAL_VITAL.CD_ATENDIMENTO = 496
+                                                                                            GROUP BY CD_ATENDIMENTO
+                                                                                          )                                                                
