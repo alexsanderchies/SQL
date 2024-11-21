7a9fc2ac9c4a661a60c2587c2ef5648739653de2
alexsandermv <alexsander.chies@mv.com.br>
2024-11-21 18:18:24 -0300

    ...

--- EDITOR/Validar campo com query.sql
+++ EDITOR/Validar campo com query.sql
@@ -3,3 +3,9 @@ select case
   when trim('&<ds_nome_1>') is null  and
            '&<rb_com_quem_outro_1>' = 'true' then 'N'
   else 'S' end from dual
+
+--validação com RB
+Select case
+when '&<RB_1_ANAMNESE>' = 'true' OR
+'&<RB_1_EVOLUCAO>' = 'true'
+then 'S' Else 'N' end from dual
